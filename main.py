075d17376b93cd17f122a133f4aa2c3568e60821
Ricardo Escalante <escalante.ricardo06@gmail.com>
2026-01-14 15:54:25 -0800

    Updated Code 1/11
    * Changed file names and main to match
    * Added all the updated code

---
+++ main.py
@@ -0,0 +1,20 @@
+from pybricks.tools import hub_menu
+
+# Make a menu to choose a letter. You can also use numbers.
+selected = hub_menu("1", "2", "3" , "4" , "5" , "6", "7","0")
+
+# Based on the selection, run a program.
+if selected == "1":
+    import Mission_5_6_7
+elif selected == "2":
+    import Mission_9_10
+elif selected == "3":
+    import Mission_8
+elif selected == "4":
+    import Mission_11
+elif selected == "5":
+    import Mission_12
+elif selected == "6":
+    import Mission_1
+elif selected == "7":
+    import Mission_2_4
